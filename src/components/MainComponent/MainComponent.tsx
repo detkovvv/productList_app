9b246284805b1ad8c7ffe7fb733452e81b650c2d
detkov <fgh.detkov@gmail.com>
2024-03-06 20:03:05 +0300

    refactoring types

--- src/components/MainComponent/MainComponent.tsx
+++ src/components/MainComponent/MainComponent.tsx
@@ -1,3 +1,4 @@
+import { type AxiosError } from 'axios';
 import { useState, useEffect, type FC } from 'react';
 
 import style from './MainComponent.module.css';
@@ -11,7 +12,7 @@ export const MainComponent: FC = () => {
     const [productsPerPage] = useState<number>(50);
     const [isLoading, setIsLoading] = useState<boolean>(false);
 
-    const getProducts = async (signal) => {
+    const getProducts = async (signal: AbortSignal) => {
         try {
             setIsLoading(true);
             const idsResponse = await axiosInstance.post(
@@ -35,7 +36,7 @@ export const MainComponent: FC = () => {
             });
             // отбор уникальных продуктов
             const uniqIds = new Set();
-            const filteredResponse = itemsResponse.data.result.filter((item) => {
+            const filteredResponse = itemsResponse.data.result.filter((item: ProductType) => {
                 if (uniqIds.has(item.id)) return false;
                 else {
                     uniqIds.add(item.id);
@@ -43,7 +44,8 @@ export const MainComponent: FC = () => {
                 }
             });
             setProducts(filteredResponse);
-        } catch (error) {
+        } catch (axiosError) {
+            const error = axiosError as AxiosError;
             if (error.name === 'AbortError') {
                 console.log('Request aborted', error.message);
             } else {
@@ -64,6 +66,14 @@ export const MainComponent: FC = () => {
         };
     }, []);
 
+    // const lastProductIndex = currentPage * productsPerPage;
+    // const firstProductIndex = lastProductIndex - productsPerPage;
+    // const currentProducts = products.slice(firstProductIndex, lastProductIndex);
+    // const paginate = (pageNumber: number) => {
+    //     setCurrentPage(pageNumber);
+    //     console.log(products, currentPage, currentProducts);
+    // };
+
     return (
         <div className={style.main_container}>
             <h1 className={style.main_title}>Product List</h1>
@@ -73,7 +83,16 @@ export const MainComponent: FC = () => {
                 <li className={style.head_item}>Price</li>
                 <li className={style.head_item}>Brand</li>
             </ul>
-            <div className={style.navigation} />
+            <div className={style.navigation}>
+                {/* <Pagination */}
+                {/*     currentPage={currentPage} */}
+                {/*     paginate={paginate} */}
+                {/*     productsPerPage={productsPerPage} */}
+                {/*     totalProducts={products.length} */}
+                {/* /> */}
+                {/* <button className={`${style.btn_back & style.btn}`}>{'prev page'}</button> */}
+                {/* <button className={`${style.btn_forward & style.btn}`}>{'next page'}</button> */}
+            </div>
             <ul className={style.list}>
                 {products.map((product) => (
                     <Product key={product.id} product={product} />
