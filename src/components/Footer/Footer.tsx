7916a0ff33ca16071bcdbb09f768f7dd2b8231a4
detkov <fgh.detkov@gmail.com>
2024-03-11 15:38:48 +0300

    refactoring

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,5 +3,9 @@ import { type FC } from 'react';
 import style from './Footer.module.css';
 
 export const Footer: FC = () => {
-    return <footer />;
+    return (
+        <footer className={style.container}>
+            <div className={style.copyright}>© 2023 detkovvv</div>
+        </footer>
+    );
 };
