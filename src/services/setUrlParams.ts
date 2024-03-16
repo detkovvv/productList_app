c3a3c32b6dd27e5f0f52104f4a617be8a35ec428
detkov <fgh.detkov@gmail.com>
2024-03-16 15:46:36 +0300

    add handleFetchError

--- src/services/setUrlParams.ts
+++ src/services/setUrlParams.ts
@@ -1,18 +1,12 @@
 export const setUrlParams = (key: string, value: string) => {
     const urlSearchParams = new URLSearchParams(window.location.search);
-    if (urlSearchParams.has(key)) {
-        key === 'page' && value === '1'
-            ? urlSearchParams.delete(key)
-            : urlSearchParams.set(key, value);
+
+    if ((key === 'page' && value === '1') || value === '') {
+        urlSearchParams.delete(key);
     } else {
-        if (key === 'page') {
-            const newUrlSearchParams = new URLSearchParams();
-            newUrlSearchParams.append(key, value);
-            const newURL = `${window.location.pathname}?${newUrlSearchParams.toString()}&${urlSearchParams.toString()}`;
-            window.history.pushState({ path: newURL }, '', newURL);
-            return;
-        } else urlSearchParams.append(key, value);
+        urlSearchParams.set(key, value);
     }
+
     const newURL = `${window.location.pathname}?${urlSearchParams.toString()}`;
     window.history.pushState({ path: newURL }, '', newURL);
 };
